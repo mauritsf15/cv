b2d7216230a968ce2ecf1cfc205c5acbdf91ac36
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2023-03-06 22:53:27 +0100

    upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const profileImg = document.querySelector('.intro-img');
+const pictures = ['one.jpg', 'two.jpg', 'three.jpg', 'four.jpg']
+
+let currentImg = 0;
+
+function rotateImg() {
+    currentImg += 1;
+    if (currentImg > 3) {
+        currentImg = 0;
+    }
+    profileImg.src = '/img/' + pictures[currentImg];
+}
+
+profileImg.addEventListener('click', rotateImg);
